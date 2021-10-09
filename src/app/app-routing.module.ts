d070616a65950851193cf5f1c2dcfbbc8610a5db
snopbear <snopbear@hotmail.com>
2021-10-09 08:00:36 -0700

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { EnquireComponent } from './enquire/enquire.component';
-import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './shared/serivces/basic/guard/auth-guard/auth.guard';
 
-// const routes: Routes = [
-//   { path: 'login', component: LoginComponent },
-//   { path: 'enquire', canActivate: [AuthGuard], component: EnquireComponent },
-//   { path: '**', redirectTo: 'login' },
-// ];
+
 
 const routes: Routes = [
 
